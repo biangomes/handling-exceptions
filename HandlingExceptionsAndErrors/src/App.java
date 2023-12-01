c1bf929b16167d9b21edfede0cc0e8e1baea4793
biangomes <beanascigom@gmail.com>
2023-11-30 21:54:06 -0300

    Gerar erro StackOverFlow e criacao do .gitignore

---
+++ HandlingExceptionsAndErrors/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        recursividade();
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+}
