fe95e8ad8d89eca486238e571d6c8cc8af9c35ef
biangomes <beanascigom@gmail.com>
2023-11-30 22:06:55 -0300

    Gerar ArrayIndexOutOfBounds

--- HandlingExceptionsAndErrors/src/runtime/test/RuntimeExceptionTest01.java
+++ HandlingExceptionsAndErrors/src/runtime/test/RuntimeExceptionTest01.java
@@ -3,7 +3,7 @@ package runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         // Checked e Unchecked
-        Object object = null;
-        object.toString();
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);    // ArrayIndexOutOfBounds
     }
 }
